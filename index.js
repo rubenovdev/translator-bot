68c1ccf20fd735de0f344ef04b22f0f56e54b020
Alexander Rubenov <alexander.rubenov@gmail.com>
2020-11-15 00:17:53 +0300

    Добавил свой id

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api')
 const fetch = require('node-fetch')
+
 const TOKEN = '1457856506:AAFAz3MNayQiPc7h0Xe8ZpTYBrdnyOU4vyw'
 const bot = new TelegramBot(TOKEN, { polling: true })
 
@@ -7,14 +8,17 @@ const IAM_TOKEN =
   't1.9euelZqPnJ3JjZqek8qYz5CZlJGTnu3rnpWaiszLy46cnYuNlZLMy5GSjZHl8_dDcEAC-u8nFXpE_t3z9wMfPgL67ycVekT-.saztk7UmBaIiCV6P9qj1iJ9jsL05_9MeVqP4ULwmwlB41Lf2AVA3xWPcYLpjDPSDdtqROFdliWTaoMLHKRZIDw'
 const FOLDER_ID = 'b1gm1cj4shlranfpo7i1'
 const URL = 'https://translate.api.cloud.yandex.net/translate/v2/translate'
-
 const RU = 'ru'
 
 bot.on('message', (msg) => {
-  if (msg.from.id === 237089463) {
+  if (msg.from.id === 237089463 || msg.from.id === 301723507) {
     const { text } = msg
 
-    if (text.length < 4 || text.split(' ').length < 2 || !text.toLowerCase().match(/[a-z]/)) {
+    if (
+      text.length < 4 ||
+      text.split(' ').length < 2 ||
+      !text.toLowerCase().match(/[a-z]/)
+    ) {
       return
     }
 
@@ -34,43 +38,9 @@ bot.on('message', (msg) => {
       },
       body: JSON.stringify(data),
     })
-      .then((response) => {
-        console.log('response: ', response)
-        return response.json()
-      })
-      .then((responseData) => {
+      .then((response) => response.json())
+      .then((responseData) =>
         bot.sendMessage(chatId, responseData.translations[0].text)
-      })
+      )
   }
 })
-
-// bot.onText(/мда/, (msg) => {
-//   console.log(msg)
-// })
-
-// {
-//   message_id: 63832,
-//   from: {
-//     id: 237089463,
-//     is_bot: false,
-//     first_name: '𝒟𝒾𝓂𝒶 ��𝑔𝑒𝓇'        ,
-//     username: 'wowyoulooksosad'
-//   },
-//   chat: { id: -1001438237715, title: 'питер', type: 'supergroup' },
-//   date: 1605355164,
-//   text: '/start',
-//   entities: [ { offset: 0, length: 6, type: 'bot_command' } ]
-// }
-
-// message_id: 63913,
-// from: {
-//   id: 301723507,
-//   is_bot: false,
-//   first_name: 'Александр',
-//   last_name: 'Рубенов',
-//   username: 'rubenovdev'
-// },
-// chat: { id: -1001438237715, title: 'питер', type: 'supergroup' },
-// date: 1605358073,
-// text: '/дай мой id'
-// }
---
+++ info.js
@@ -0,0 +1,3 @@
+// chatId = -1001438237715
+// egerId = 237089463 || wowyoulooksosad
+// myId = 301723507 || rubenovdev
