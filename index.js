38f0568cc1b49f7ca4f747ed6be8aa6d6622db56
Alexander Rubenov <alexander.rubenov@gmail.com>
2020-11-16 01:37:55 +0300

    добавил режимы PROD TEST

--- index.js
+++ index.js
@@ -3,17 +3,27 @@ const fetch = require('node-fetch')
 
 const TOKEN = '1457856506:AAFAz3MNayQiPc7h0Xe8ZpTYBrdnyOU4vyw'
 const bot = new TelegramBot(TOKEN, { polling: true })
+const PROD = 'production'
+const TEST = 'development'
 
 let iamToken
+let mode = PROD
 const FOLDER_ID = 'b1gm1cj4shlranfpo7i1'
 const TRANSLATE_URL =
   'https://translate.api.cloud.yandex.net/translate/v2/translate'
 const IAM_TOKENS_URL = 'https://iam.api.cloud.yandex.net/iam/v1/tokens'
-const RU = 'ru'
 const YANDEX_OAUTH_TOKEN = 'AgAAAAAs6XH4AATuwbCv3VlS2U8eilL83bCCoP8'
 const DEFAULT_CHAT_ID = -1001438237715
+const RU = 'ru'
 
 function updateIamToken() {
+  if (mode === TEST) {
+    bot.sendMessage(
+      DEFAULT_CHAT_ID,
+      '- отправка запроса на получение iam-токена'
+    )
+  }
+
   fetch(IAM_TOKENS_URL, {
     method: 'POST',
     body: JSON.stringify({
@@ -21,15 +31,22 @@ function updateIamToken() {
     }),
   })
     .then((response) => response.json())
-    .then(({ iamToken: newIamToken }) => {
-      iamToken = newIamToken
+    .then((responseData) => {
+      if (mode === TEST) {
+        bot.sendMessage(
+          DEFAULT_CHAT_ID,
+          `- ответ от сервера: ${JSON.stringify(responseData)}`
+        )
+      }
+
+      iamToken = responseData.iamToken
     })
-    .catch((error) =>
+    .catch((error) => {
       bot.sendMessage(
         DEFAULT_CHAT_ID,
         `Ошибка при получении iam-токена: ${error.name}. Описание: ${error.message}`
       )
-    )
+    })
 }
 
 updateIamToken()
@@ -37,7 +54,9 @@ updateIamToken()
 setInterval(() => updateIamToken(), 360000)
 
 bot.on('message', (msg) => {
-  if (msg.from.id === 237089463 || msg.from.id === 301723507) {
+  const fromId = msg.from.id
+
+  if (fromId === 237089463 || fromId === 301723507) {
     const { text } = msg
 
     if (
@@ -66,19 +85,44 @@ bot.on('message', (msg) => {
     })
       .then((response) => response.json())
       .then((responseData) => {
-        if (
-          responseData &&
-          responseData.translations &&
-          responseData.translations.length
-        ) {
+        if (mode === TEST) {
+          bot.sendMessage(
+            chatId,
+            `- ответ от сервера: ${JSON.stringify(responseData)}`
+          )
+        }
+
+        if (responseData && responseData.translations) {
           bot.sendMessage(chatId, responseData.translations[0].text)
         }
       })
-      .catch((error) =>
+      .catch((error) => {
         bot.sendMessage(
           chatId,
           `Ошибка при переводе сообщения: ${error.name}. Описание: ${error.message}`
         )
-      )
+      })
   }
+})
+
+bot.onText(/test/, (msg) => {
+  const chatId = msg.chat.id
+
+  if (msg.from.id !== 301723507) {
+    bot.sendMessage(chatId, '- нет доступа')
+  }
+
+  mode = TEST
+  bot.sendMessage(chatId, '- включен режим development')
+})
+
+bot.onText(/prod/, (msg) => {
+  const chatId = msg.chat.id
+
+  if (msg.from.id !== 301723507) {
+    bot.sendMessage(chatId, '- нет доступа')
+  }
+
+  mode = PROD
+  bot.sendMessage(chatId, '- включен режим production')
 })
